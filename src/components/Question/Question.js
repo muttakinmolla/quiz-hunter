64769f3307ebb197806877350493a6ebeaf14c24
muttakin1991 <shawonmuttakin@gmail.com>
2022-10-11 02:11:48 +0600

    react toast setup done

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import Options from '../Options/Options';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ qstn }) => {
     const { options, question, correctAnswer } = qstn;
-    const [answer, setAnswer] = useState(correctAnswer);
+    // const [answer, setAnswer] = useState(correctAnswer);
 
     const handleCorrectAnswer = (option) => {
-        if (option === answer) {
-            alert('ok')
+        if (option === correctAnswer) {
+            correctToast();
         } else {
             alert('false')
         }
     }
+
+    const correctToast = () => toast.success('🦄 Wow so easy!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
 
     return (
         <div>
@@ -20,6 +33,18 @@ const Question = ({ qstn }) => {
             {
                 options.map(option => <Options option={option} handleCorrectAnswer={handleCorrectAnswer} key={option}></Options>)
             }
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
         </div>
     );
 };
