5ca6fb3f435fe201dcee512de24c8da7e17cd7d6
muttakin1991 <shawonmuttakin@gmail.com>
2022-10-10 23:51:47 +0600

    project setup done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand bg-info p-2 rounded text-white" to="#">QUIZ HUNTER</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/blog">blog</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/static">static</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Home = () => {
+    return (
+        <div className='main-header'>
+            <div className='header'>
+                <h1>Quiz hunter</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
